991d5b7d32767669a23915cf061be729c91d65f3
Roman Chursanov <authfailed@gmail.com>
2024-11-01 18:36:34 +0500

    Переход на node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://chrsnv.ru",
-  integrations: [
-    mdx(),
-    sitemap(),
-    solidJs(),
-    tailwind({ applyBaseStyles: false }),
-  ],
+    site: "https://chrsnv.ru",
+    output: "server",
+    adapter: node({
+        mode: "standalone"
+    }),
+    integrations: [
+        mdx(),
+        sitemap(),
+        solidJs(),
+        tailwind({applyBaseStyles: false}),
+    ],
 });
