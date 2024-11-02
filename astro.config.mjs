cc16d917e06f514e57307ee67eed432387f3bc94
Roman Chursanov <authfailed@gmail.com>
2024-11-02 22:54:38 +0500

    Обновление output на hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
     site: "https://chrsnv.ru",
-    output: "server",
+    output: "hybrid",
     adapter: node({
         mode: "standalone"
     }),
