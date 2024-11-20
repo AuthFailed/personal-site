1d8d493fccb735bbeca83e81358c0a22353ba3ee
Roman Chursanov <authfailed@gmail.com>
2024-11-20 16:22:55 +0500

    Смена output на static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://chrsnv.ru",
-    output: "hybrid",
-    adapter: node({
-        mode: "standalone"
-    }),
+    output: "static",
     integrations: [
         mdx(),
         sitemap(),
